e4ad93ee4b4d600aacce38d4766425c520219a4b
Sergio <sergioburca@gmail.com>
2018-12-13 15:57:44 -0500

    One element Selected we travers and modifie DOM elements using CSS selectors and methods

--- base/js/script.js
+++ base/js/script.js
@@ -9,10 +9,16 @@ $(function () {
   // $(".blue-box").animate({   "margin-left" : "-=200px" },1000, "linear");
   // $('p').animate({   fontSize : "20px" }, 1000) $('.lightbox').fadeIn(1000)
 
-  $('p').css('background-color', "rgba(180, 180, 40, .8")
-  $('.red-box').css('color', "rgba(180, 180, 40, .8")
-  $('.blue-box').css('background-color', "rgba(180, 180, 40, .8")
-  $('#list').css('background-color', "rgba(180, 180, 40, .8")
+  // $('p').css('background-color', "rgba(180, 180, 40, .8")
+  // $('.red-box').css('color', "rgba(180, 180, 40, .8")
+  // $('.blue-box').css('background-color', "rgba(180, 180, 40, .8")
+  $('#list').find("li").css('background-color', "rgba(100, 180, 40, .8")
+  $('#list').children("li").css('background-color', "rgba(80, 100, 40, .8")
+  $('#list').parents('body').css('background-color', "rgba(100, 100, 250, .8")
+  $('#list').parent().css('background-color', "rgba(250, 100, 80, .9")
+  $('#list').siblings(':header').css('background-color', "rgba(250, 205, 40, .8")
+  // $('#list').css('background-color', "rgba(180, 180, 40, .8")
+  // $('#list').css('background-color', "rgba(180, 180, 40, .8")
 
 
 
