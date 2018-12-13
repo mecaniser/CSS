b80ce6fce720509863e8f61f1b3c3cf189d989b1
Sergio <sergioburca@gmail.com>
2018-12-13 15:45:50 -0500

    Selecting multiple classes or elements to be modified

--- base/js/script.js
+++ base/js/script.js
@@ -1,30 +1,19 @@
-$(function() {
-  // jQuery goes here...
+$(function () {
+  // jQuery goes here... Un comment this line to fade out the red box on page load
+  // $(".red-box").fadeTo(2000, 0.2); $(".green-box").fadeTo(2000, 0.5);
+  // $(".blue-box").fadeTo(2000, .8); $(".red-box").fadeTo(2000, 0.9);
+  // $(".green-box").fadeTo(2000, 0.6); $(".blue-box").fadeTo(2000, .3);
+  // $(".green-box").fadeToggle(); $(".blue-box").slideUp();
+  // $(".blue-box").slideDown(2000); $(".blue-box").animate({   "margin-left" :
+  // "+=200px" },1000, "linear"); $(".blue-box").fadeTo(1000, 0.9);
+  // $(".blue-box").animate({   "margin-left" : "-=200px" },1000, "linear");
+  // $('p').animate({   fontSize : "20px" }, 1000) $('.lightbox').fadeIn(1000)
 
-  // Un comment this line to fade out the red box on page load
-  $(".red-box").fadeTo(2000, 0.2);
-  $(".green-box").fadeTo(2000, 0.5);
-  $(".blue-box").fadeTo(2000, .8);
-  $(".red-box").fadeTo(2000, 0.9);
-  $(".green-box").fadeTo(2000, 0.6);
-  $(".blue-box").fadeTo(2000, .3);
+  $('p').css('background-color', "rgba(180, 180, 40, .8")
+  $('.red-box').css('color', "rgba(180, 180, 40, .8")
+  $('.blue-box').css('background-color', "rgba(180, 180, 40, .8")
+  $('#list').css('background-color', "rgba(180, 180, 40, .8")
 
-  // $(".green-box").fadeToggle();
-
-  $(".blue-box").slideUp();
-  $(".blue-box").slideDown(2000);
-  $(".blue-box").animate({
-    "margin-left" : "+=200px"
-  },1000, "linear");
-  $(".blue-box").fadeTo(1000, 0.9);
-  $(".blue-box").animate({
-    "margin-left" : "-=200px"
-  },1000, "linear");
-  $('p').animate({
-    fontSize : "20px"
-  }, 1000)
-
-// $('.lightbox').fadeIn(1000)
 
 
 });
