13753ccacc6af462a06c31d1f3165c338b838ead
Sergio <sergioburca@gmail.com>
2018-12-13 18:12:57 -0500

    Replacing elements practice

--- base/js/script.js
+++ base/js/script.js
@@ -8,18 +8,21 @@ $(function () {
   // "+=200px" },1000, "linear"); $(".blue-box").fadeTo(1000, 0.9);
   // $(".blue-box").animate({   "margin-left" : "-=200px" },1000, "linear");
   // $('p').animate({   fontSize : "20px" }, 1000) $('.lightbox').fadeIn(1000)
-
   // $('p').css('background-color', "rgba(180, 180, 40, .8")
   // $('.red-box').css('color', "rgba(180, 180, 40, .8")
   // $('.blue-box').css('background-color', "rgba(180, 180, 40, .8")
-  $('#list').find("li").css('background-color', "rgba(100, 180, 40, .8")
-  $('#list').children("li").css('background-color', "rgba(80, 100, 40, .8")
-  $('#list').parents('body').css('background-color', "rgba(100, 100, 250, .8")
-  $('#list').parent().css('background-color', "rgba(250, 100, 80, .9")
-  $('#list').siblings(':header').css('background-color', "rgba(250, 205, 40, .8")
-  // $('#list').css('background-color', "rgba(180, 180, 40, .8")
+  // $('#list').find("li").css('background-color', "rgba(100, 180, 40, .8")
+  // $('#list').children("li").css('background-color', "rgba(80, 100, 40, .8")
+  // $('#list').parents('body').css('background-color', "rgba(100, 100, 250, .8")
+  // $('#list').parent().css('background-color', "rgba(250, 100, 80, .9")
+  // $('#list').siblings(':header').css('background-color', "rgba(250, 205, 40,
+  // .8") $('#list').css('background-color', "rgba(180, 180, 40, .8")
   // $('#list').css('background-color', "rgba(180, 180, 40, .8")
 
+  // $("ul ul:first").append('<li>I\'m the last element');
+  // $('<li>Some text inside the list element<li>').appendTo($('ul ul:first'))
+  $('.red-box').replaceWith('<div class="green-box">was replace with green box</div>');
+  $('.blue-box').replaceWith('<div class="green-box">was replace with green box</div>');
 
 
 });
